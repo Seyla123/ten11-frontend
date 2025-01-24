de03c6d7bd242dfa75f90e47639d112f2eb04ee3
Seav Seyla <Mrrseyla.758@gmail.com>
2025-01-24 15:41:27 +0700

    feat(navbar) : finish navbar

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -1,3 +1,20 @@
-<nav>
-    
+<nav class="bg-green-600">
+    <div class="flex justify-between px-4 items-center h-20 max-w-screen-2xl mx-auto">
+        {{-- hamburger menu and logo --}}
+        <div class="flex gap-4 w-full">
+            <x-icons.hamberger-menu/>
+            <x-logo/>
+        </div>
+        <div class="hidden sm:flex w-full justify-center">
+            <x-big-logo/>
+        </div>
+        {{-- other menu --}}
+        <div class="flex justify-end gap-4 items-center w-full">
+            <x-icons.search/>
+            <x-icons.ring/>
+            <x-icons.bag/>
+            <x-icons.watchlist/>
+            <x-icons.arrow-down/>
+        </div>
+    </div>
 </nav>
