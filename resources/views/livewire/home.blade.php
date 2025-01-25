b99594c21d3c9f9a6ff4a90361ccfdf2d8d99c77
Seav Seyla <Mrrseyla.758@gmail.com>
2025-01-25 18:57:08 +0700

    feat(navbar) : finish navbar

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,7 +1,7 @@
 <main>
     <livewire:hero-section />
     <div class="max-w-screen-xl mx-auto px-2 mb-12">
-        <livewire:collection-section/>
-        <livewire:highlight-section/>
+        <livewire:collection-section />
+        <livewire:highlight-section />
     </div>
 </main>
--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -5,7 +5,7 @@
             <x-icons.hamberger-menu />
             <x-logo />
         </div>
-        <div class="hidden sm:flex w-full justify-center">
+        <div class="hidden sm:flex w-full justify-center" x-ref='search'>
             <x-big-logo />
         </div>
         {{-- other menu --}}
@@ -18,7 +18,6 @@
             <x-icons.watchlist />
             {{-- hide when big screen --}}
             <x-icons.arrow-down class="sm:hidden" />
-
             {{-- show when big screen --}}
             @if ($userName)
                 <div class="uppercase hidden sm:flex justify-center items-center gap-1">
@@ -29,15 +28,15 @@
         </div>
     </div>
     {{-- search --}}
-    <div x-show="open" @click.outside="open = false" class="bg-white fixed h-[80%] w-full z-10 top-0 px-2">
+    <div x-data='{placeholder:"What is you searhing for ?"}' @click.outside="open = false" x-show="open"
+        x-transition.duration.500ms class="bg-white fixed h-[80%] w-full z-10 top-0 px-2">
         <div class="border-b border-black max-w-screen-xl mx-auto mt-4 py-2  flex">
-            <input type="text" class="w-full focus:outline-none  focus:ring-0  text-xl"
-                placeholder="What is you searhing for ?">
+            <input type="text" class="w-full focus:outline-none  focus:ring-0  text-xl" :placeholder="placeholder">
             <div class="flex gap-2 xl:gap-4">
                 <x-icons.search />
-                <button @click="open = false" >
+                <button @click="open = false">
                     x
-                </span>
+                </button>
             </div>
         </div>
     </div>
