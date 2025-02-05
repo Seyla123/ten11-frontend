6331326e1365f28792b5216d4deda3ecaa1be8d3
Seav Seyla <Mrrseyla.758@gmail.com>
2025-02-05 20:50:03 +0700

    fix(search-navbar) : fix trasalte styles

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -28,8 +28,15 @@
         </div>
     </div>
     {{-- search --}}
-    <div x-data='{placeholder:"What is you searhing for ?"}' @click.outside="open = false" x-show="open"
-        x-transition.duration.500ms class="bg-white fixed h-[80%] w-full z-10 top-0 px-2">
+    <div x-data='{placeholder:"What is you searhing for ?"}' @click.outside="open = false" 
+        x-show="open"
+        x-transition:enter="transition ease-out duration-200"
+        x-transition:enter-start="-translate-y-full"
+        x-transition:enter-end="translate-y-0"
+        x-transition:leave="transition ease-in duration-200"
+        x-transition:leave-start="translate-y-0"
+        x-transition:leave-end="-translate-y-full"
+        class="bg-white fixed h-[80%] w-full z-10 top-0 px-2">
         <div class="border-b border-black max-w-screen-xl mx-auto mt-4 py-2  flex">
             <input type="text" class="w-full focus:outline-none  focus:ring-0  text-xl" :placeholder="placeholder">
             <div class="flex gap-2 xl:gap-4">
